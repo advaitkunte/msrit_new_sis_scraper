4aec9f8d854e9846b7666b057b09f659d98595ca
Advait Kunte <advaitkunte@gmail.com>
2014-10-09 17:32:19 +0530

    fixed sis details

--- get_data.py
+++ get_data.py
@@ -2,12 +2,12 @@
 
 from bs4 import BeautifulSoup
 import json
+import time
 
 # for today's timetable
 def timetable(row):
     date = row[1].string.replace('Timetable','').strip()
     subs = []
-    
     if not str(row[2]).find('No Classes Scheduled today!') > 0:
         for timetable in row[2].findAll('li'):
             data = {}
@@ -25,39 +25,62 @@ def timetable(row):
     data = {'subjects':subs,'date':date}
     return data
 
-def details(row):
+def sis(table):
     subs = []
-    for details in row[5].findAll('div',{'class':'big_container'}):
+    for tab in table.findAll('div',{'class':'big_container'}):
         data = {}
-        t = details.findAll('td')
-        attendance = t[9].findAll('span')
-        try:
-            data['attended'] = attendance[0].string.strip()
-        except Exception as e:
+        basic = tab.find('table').findAll('td')
+        
+        # basic
+        data['sub_code'] = basic[0].string.strip()
+        data['sub_name'] = basic[1].string.strip()
+        
+        # 
+        attendance = tab.find('div',{'class':'boxmiddle'}).findAll('tr')
+        marks = tab.find('div',{'class':'boxright'}).findAll('tr')
+        details = tab.find('div',{'class':'boxleft'}).findAll('tr')
+        
+        # attendance
+        data['percentage'] = attendance[2].string.strip()
+        if attendance[4].findAll('span')[0].string:
+            data['attended'] = int(attendance[4].findAll('span')[0].string)
+        else:
             data['attended'] = 0
-        try:
-            data['conducted'] = attendance[1].string.strip()
-        except Exception as e:
+        
+        if attendance[4].findAll('span')[1].string:
+            data['conducted'] = int(attendance[4].findAll('span')[1].string)
+        else:
             data['conducted'] = 0
-        data['code'] = t[0].string.strip()
-        data['name'] = t[1].string.strip()
-        data['attendance_percentage'] = t[7].string.strip()
-        data['credits'] = t[14].string.strip()
-        data['course_type'] = t[16].string.strip()
-        data['course_nature'] = t[18].string.strip()
-        data['cie_max_marks'] = t[20].string.strip()
-        data['see_max_marks'] = t[22].string.strip()
-        subs.append(data)
+        
+        # details
+        data['credits'] = details[4].string.strip()
+        data['type'] = details[6].string.strip()
+        data['nature'] = details[8].string.strip()
+        data['cie_max'] = float("%0.2f" % float(details[10].string.strip()))
+        data['see_max'] = float("%0.2f" % float(details[12].string.strip()))
 
+
+        # CIE marks
+        if len(marks) < 3:
+            data['T1'] = float("%0.2f" % float(-1))
+            data['T2'] = float("%0.2f" % float(-1))
+            data['T3'] = float("%0.2f" % float(-1))
+        else:
+            # TODO 
+            pass
+        subs.append(data)
     return subs
 
-def main():
-    with open ("response.html", "r") as myfile:
-        data=myfile.read()
+def main(data):
     bs = BeautifulSoup(data)
     table = bs.find("td",{'width':'60%','style':'vertical-align:top;padding:0px 5px 0px 5px;border-right:1px dashed black'})
-    row = table.findAll('tr')
-    tt = timetable(row)
-    det = details(row)
-    final = {'timetable':tt, 'details':det}
+    # table = bs.find('div',{'id':'left-column'}).find('table',{'width':'980px'}).findAll('tr')[2].find('td',{'style':'vertical-align:top;padding:0px 5px 0px 5px;border-right:1px dashed black'})
+
+    tt = timetable(table.findAll('tr'))
+    det = sis(table)
+    final = {'timetable':tt, 'sis':det, 'status':200, 'desc':'success'}
+    
     print json.dumps(final,indent = 4)
+
+file = open('response-'+time.strftime("%d_%m_%Y")+'.html', 'r')
+main(file.read())
--- login.py
+++ login.py
@@ -6,27 +6,9 @@ import urllib
 import urllib2
 from bs4 import BeautifulSoup
 import base64,cookielib
-import get_data
-
-username = ""
-password = ""
-
-url = 'http://parents.msrit.edu/index.php'
-user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.94 Safari/537.36'
-cj = cookielib.CookieJar()
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-
-# to get first cookie and other parameters
-response = opener.open(url)
-
-token = []
-val = ''
-bs = BeautifulSoup(response.read())
-table = bs.find("table").find('tr').findAll('input')
-
-val = table[len(table)-3].get('value')
-token.append(table[len(table)-1].get('name'))
-token.append(table[len(table)-1].get('value'))
+# import get_data
+import json
+import time
 
 def anotherfunction():
     n = math.floor(random.random()*62)
@@ -43,44 +25,62 @@ def randomstring(L):
         s += str(anotherfunction())
     return s
 
-tmp = []
+username = ""
+password = ""
 
-randsting = ''
-for i in range(0,len(password)):
-    randsting += password[i]+randomstring(2)
+url = 'http://parents.msrit.edu/index.php'
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.94 Safari/537.36'
+cj = cookielib.CookieJar()
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
 
-# getting encoded password
-encoded = base64.standard_b64encode(randsting)
+try:
+    # to get first cookie and other parameters
+    response = opener.open(url)
 
-# building the POST param
-values = {
-          'username' : username,
-          'password' : encoded,
-          'passwd' : encoded,
-          'option' : 'com_user',
-          'task' : 'login',
-          'remember' : 'No',
-          'return' : val,
-          'return' : '',
-          token[0] : token[1]
-          }
-data = urllib.urlencode(values)
-# getting login cookie
-response = opener.open(url, data)
-url = response.geturl()
+    token = []
+    val = ''
+    bs = BeautifulSoup(response.read())
+    table = bs.find("table").find('tr').findAll('input')
 
+    val = table[len(table)-3].get('value')
+    token.append(table[len(table)-1].get('name'))
+    token.append(table[len(table)-1].get('value'))
 
-values = { 'option' : 'com_studentdashboard',
-           'controller' : 'studentdashboard',
-           'task' : 'dashboard'
-          }
-data = urllib.urlencode(values)
-# getting the initial page
-response = opener.open(url)
-the_page = response.read()
+    randsting = ''
+    for i in range(0,len(password)):
+        randsting += password[i]+randomstring(2)
 
-file = open("response.html", "w")
-file.write(the_page)
-file.close()
+    # getting encoded password
+    encoded = base64.standard_b64encode(randsting)
 
-get_data.main()
+    # building the POST param
+    values = {
+              'username' : username,
+              'password' : encoded,
+              'passwd' : encoded,
+              'option' : 'com_user',
+              'task' : 'login',
+              'remember' : 'No',
+              'return' : val,
+              'return' : '',
+               token[0] : token[1]
+              }
+    data = urllib.urlencode(values)
+    # getting login cookie
+    response = opener.open(url, data)
+    url = response.geturl()
+    the_page = response.read()
+
+    if url == 'http://parents.msrit.edu/index.php':
+        data = {'status':1,'desc':'Invalid Username / Password'}
+        print json.dumps(data,indent = 4)
+    else:
+        filename = 'response-'+time.strftime("%d_%m_%Y")+'.html'
+        f = open(filename, 'w')
+        f.write(the_page)
+        f.close()
+        data = {'status':200, 'desc':'success'}
+        print json.dumps(data,indent = 4)
+except Exception as e:
+    data = {'status':500, 'desc':str(e.reason)}
+    print json.dumps(data,indent = 4)
